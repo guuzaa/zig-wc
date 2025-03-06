025531469d298cfb40b2eaeaad6228f6afeccdb4
guza <guza@duck.com>
2025-03-06 20:28:46 +0800

    fix: tidy build.zig

--- build.zig
+++ build.zig
@@ -76,23 +76,8 @@ pub fn build(b: *std.Build) void {
     // This creates a build step. It will be visible in the `zig build --help` menu,
     // and can be selected like this: `zig build run`
     // This will evaluate the `run` step rather than the default, which is "install".
-    const run_step = b.step("run", "Run the app");
+    const run_step = b.step("run", "Run the binary");
     run_step.dependOn(&run_cmd.step);
-
-    // Creates a step for unit testing.
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    // Add module dependencies to the test
-    exe_unit_tests.root_module.addImport("counter", counter_module);
-    exe_unit_tests.root_module.addImport("options", options_module);
-    exe_unit_tests.root_module.addImport("formatter", formatter_module);
-    exe_unit_tests.root_module.addImport("cli", cli_module);
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
     // Create separate test steps for each module
     const counter_tests = b.addTest(.{
@@ -147,8 +132,7 @@ pub fn build(b: *std.Build) void {
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
+    const test_step = b.step("test", "Execute all unit and integration tests");
     test_step.dependOn(&run_counter_tests.step);
     test_step.dependOn(&run_options_tests.step);
     test_step.dependOn(&run_cli_tests.step);
